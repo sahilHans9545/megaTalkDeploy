36263458897020f08e862893cf0004876e558bcd
Sahil <hanss9545@gmail.com>
2023-11-30 01:00:02 +0530

    vite.config file changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+const defineConfig = {
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#big-list-of-options
+    },
+  },
+};
+export default defineConfig;
