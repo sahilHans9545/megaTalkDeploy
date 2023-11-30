eb2eb7e7ac714c536cecda98d4310edc90338886
Sahil <hanss9545@gmail.com>
2023-11-30 20:36:47 +0530

    file name changed

--- server/index.js
+++ server/index.js
@@ -22,9 +22,9 @@ app.use("/api", router);
 
 const __dirname1 = path.join(__dirname, "../");
 console.log(__dirname1);
-console.log(path.join(__dirname1, "/frontEnd/dist"));
+console.log(path.join(__dirname1, "/frontend/dist"));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontEnd/dist")));
+  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "frontEnd", "dist", "index.html"));
   });
