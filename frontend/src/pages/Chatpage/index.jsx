7b10800913cb4daa0acef8fbee9abab9abc59256
Sahil <hanss9545@gmail.com>
2024-01-11 07:45:28 +0530

    chat responsive  css

--- frontend/src/pages/Chatpage/index.jsx
+++ frontend/src/pages/Chatpage/index.jsx
@@ -233,7 +233,7 @@ const ChatPage = () => {
         <div
           className={`flex-1 ${
             selectedChat ? "block" : "hidden md:block"
-          } h-screen md:h-auto fixed md:static top-0 left-0`}
+          } h-screen md:h-auto fixed md:static top-0 left-0 right-0`}
           //  fixed sm:static top-0 bottom-0 left-0 right-0 h-[100vh] sm:h-auto
         >
           <ChatBox socket={socket} />
