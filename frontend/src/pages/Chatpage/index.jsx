86dfa2053a9e92a2692705e8f3754f14545e9d51
Sahil <hanss9545@gmail.com>
2024-01-11 07:27:32 +0530

    chat responsive  css

--- frontend/src/pages/Chatpage/index.jsx
+++ frontend/src/pages/Chatpage/index.jsx
@@ -209,7 +209,7 @@ const ChatPage = () => {
   return (
     <div
       id="chatPage"
-      className="flex flex-col"
+      className="flex flex-col fixed top-0 left-0 w-full"
       style={{ userSelect: "revert-layer" }}
     >
       <Header setShowSideDrawer={setShowSideDrawer} />
